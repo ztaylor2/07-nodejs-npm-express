d6b92c3e2c6b6b559594c053ee13f2147418d8e9
Zach Taylor <ztaylor2@zagmail.gonzaga.edu>
2017-08-22 13:48:00 -0700

    finished first part of lab

--- starter-code/server.js
+++ starter-code/server.js
@@ -6,10 +6,13 @@
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 // There is also a package here called body-parser, which is required in for use in a new route.
 // Be sure to install that and save it as a dependency after you create your package.json.
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
 // TODO: Include all of the static resources as an argument to app.use()
+app.use(express.static('./public'));
 
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
 
@@ -24,4 +27,5 @@ app.post('/articles', bodyParser, function(request, response) {
 
 app.listen(PORT, function() {
   // TODO: Log to the console a message that lets you know which port your server has started on
+  console.log('Our port is on ' + PORT);
 });
